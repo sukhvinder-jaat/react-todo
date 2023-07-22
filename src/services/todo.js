2f5516592a1114f0598637613ad67e31a7b0496f
sukhvinder-jaat <monudhaka829@gmail.com>
2023-07-22 15:36:40 +0530

    change

--- src/services/todo.js
+++ src/services/todo.js
@@ -6,21 +6,21 @@ import update from 'immutability-helper';
  */
 export function getAll() {
     return [
-        {
-            id: 1,
-            text: 'Learn Javascript',
-            completed: false
-        },
-        {
-            id: 2,
-            text: 'Learn React',
-            completed: false
-        },
-        {
-            id: 3,
-            text: 'Build a React App',
-            completed: false
-        }
+        // {
+        //     id: 1,
+        //     text: 'TODO LIST',
+        //     completed: false
+        // },
+        // {
+        //     id: 2,
+        //     text: 'Learn React',
+        //     completed: false
+        // },
+        // {
+        //     id: 3,
+        //     text: 'Build a React App',
+        //     completed: false
+        // }
     ]
 }
 
@@ -30,7 +30,6 @@ export function getItemById(itemId) {
 
 export function updateStatus(items, itemId, completed) {
     let index = items.findIndex(item => item.id === itemId);
-
     // Returns a new list of data with updated item.
     return update(items, {
         [index]: {
